d1b8305d8fbb8210814c8eb5adc54bd9be89514d
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 06:09:45 +0300

    1

--- twocomms/twocomms/urls.py
+++ twocomms/twocomms/urls.py
@@ -2,9 +2,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", include("storefront.urls")),
+    # Явный маршрут для /favicon.ico → статический файл
+    path("favicon.ico", RedirectView.as_view(
+        url=staticfiles_storage.url("img/favicon.ico"), permanent=False
+    )),
 ]
 
 # Добавляем обработку медиа-файлов для разработки и продакшена
