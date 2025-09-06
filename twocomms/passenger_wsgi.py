87be68b2d2c9d66d32a6d5db8ccb4ffd8c1f3a0d
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 21:09:50 +0300

    1

--- twocomms/passenger_wsgi.py
+++ twocomms/passenger_wsgi.py
@@ -1,6 +1,29 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twocomms.settings")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twocomms.settings')
-
 application = get_wsgi_application()
+
+
+
+#import importlib.machinery
+#import importlib.util
+#import os
+#import sys
+
+
+#sys.path.insert(0, os.path.dirname(__file__))
+
+#def load_source(modname, filename):
+ #   loader = importlib.machinery.SourceFileLoader(modname, filename)
+ #   spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+  #  module = importlib.util.module_from_spec(spec)
+   # loader.exec_module(module)
+   # return module
+
+#wsgi = load_source('wsgi', 'passenger_wsgi.py') ааааааа
+#application = wsgi.application
