4a4654d7a72ac1065ce1f59a1519a547c1d70f9a
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 17:07:13 +0300

    1

--- twocomms/passenger_wsgi.py
+++ twocomms/passenger_wsgi.py
@@ -7,7 +7,14 @@ sys.path.insert(0, os.path.dirname(__file__))
 # Указываем модуль настроек для продакшена
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twocomms.production_settings")
 
-# WSGI-приложение для Passenger/uWSGI/Gunicorn
-from twocomms.wsgi import application
+# Импортируем Django WSGI приложение
+import django
+from django.core.wsgi import get_wsgi_application
+
+# Инициализируем Django
+django.setup()
+
+# Создаем WSGI приложение
+application = get_wsgi_application()
 
 
