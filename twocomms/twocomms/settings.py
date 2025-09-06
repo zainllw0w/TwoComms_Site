f7e276fcb61f6e74859bb8a826e2510d2411112a
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 12:58:30 +0300

    1

--- twocomms/twocomms/settings.py
+++ twocomms/twocomms/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+
+# Настройка PyMySQL для работы с MySQL
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
