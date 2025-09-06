83a279e8bbaeb69476dfbef233c025f68971eb2f
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 19:27:15 +0300

    1

--- twocomms/passenger_wsgi.py
+++ twocomms/passenger_wsgi.py
@@ -1,13 +1,6 @@
 import os
-import sys
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Используем production_settings если доступен, иначе обычные settings
-try:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twocomms.production_settings")
-except:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twocomms.settings")
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twocomms.production_settings')
+
 application = get_wsgi_application()
