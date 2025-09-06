4d7450174f91890b2eba07a7fedbdcab90a03578
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 23:57:25 +0300

    Исправлен passenger_wsgi.py: используется production_settings вместо settings для корректной работы CSRF в production

--- twocomms/passenger_wsgi.py
+++ twocomms/passenger_wsgi.py
@@ -1,6 +1,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twocomms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twocomms.production_settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
