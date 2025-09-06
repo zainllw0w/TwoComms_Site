9e9800bcd598c754b7a19f2f8d5d34ecb3741098
zainllw0w <pilimcenu270795@gmail.com>
2025-09-06 21:09:50 +0300

    1

--- twocomms/twocomms/wsgi.py
+++ twocomms/twocomms/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twocomms.settings')
 
 application = get_wsgi_application()
+
+# ЖЕЛАТЕЛЬНО, НО НЕ ОБЯЗАТЕЛЬНО
