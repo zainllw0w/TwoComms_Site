76b18244bc404feb2ed13bc51661096cf519ad1f
zainllw0w <pilimcenu270795@gmail.com>
2025-09-09 21:22:06 +0300

    Реализовано автоматическое обновление кнопки Telegram без перезагрузки страницы

--- twocomms/accounts/urls.py
+++ twocomms/accounts/urls.py
@@ -7,4 +7,5 @@ from . import telegram_views
 urlpatterns = [
     path('telegram/webhook/', telegram_views.telegram_webhook, name='telegram_webhook'),
     path('telegram/link/', telegram_views.link_telegram_account, name='link_telegram_account'),
+    path('telegram/status/', telegram_views.check_telegram_status, name='check_telegram_status'),
 ]
