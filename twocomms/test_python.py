b7c2cde194191bac3893214b359ba06254bf4957
zainllw0w <pilimcenu270795@gmail.com>
2025-09-09 17:11:37 +0300

    Удалены тестовые файлы диагностики
    - Удалены все временные файлы для диагностики 500 ошибки
    - Django приложение работает корректно (проверено тестами)
    - Проблема в конфигурации веб-сервера cPanel/HostPro

--- twocomms/test_python.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-print("Content-Type: text/html\n")
-print("<h1>Python работает!</h1>")
-print("<p>Время:", __import__('datetime').datetime.now(), "</p>")
-print("<p>Версия Python:", __import__('sys').version, "</p>")
