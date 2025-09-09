e4409008096f9ab37ffdd137931c4cddcb6338ee
zainllw0w <pilimcenu270795@gmail.com>
2025-09-09 17:11:37 +0300

    Удалены тестовые файлы диагностики
    - Удалены все временные файлы для диагностики 500 ошибки
    - Django приложение работает корректно (проверено тестами)
    - Проблема в конфигурации веб-сервера cPanel/HostPro

--- twocomms/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "PHP работает!<br>";
-echo "Текущее время: " . date('Y-m-d H:i:s') . "<br>";
-echo "Домен: " . $_SERVER['HTTP_HOST'] . "<br>";
-echo "Путь: " . $_SERVER['REQUEST_URI'] . "<br>";
-?>
