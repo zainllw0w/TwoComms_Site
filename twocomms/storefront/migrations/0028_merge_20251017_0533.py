5f5d2fb2be9051fa3c41d82e3d4c6a976572437b
zainllw0w <pilimcenu270795@gmail.com>
2025-10-20 00:12:47 +0300

    Add storefront merge migration stub

---
+++ twocomms/storefront/migrations/0028_merge_20251017_0533.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('storefront', '0027_product_dropship_note_product_is_dropship_available_and_more'),
+    ]
+
+    operations = []
