39cafd9451bfa04d111bb4b846b901c261e05007
zainllw0w <pilimcenu270795@gmail.com>
2025-10-21 21:20:24 +0300

    Фильтр abs для пользовательских баллов

--- twocomms/accounts/templatetags/math_filters.py
+++ twocomms/accounts/templatetags/math_filters.py
@@ -1,6 +1,9 @@
+from decimal import Decimal, InvalidOperation
+
 from django import template
 
 register = template.Library()
+
 
 @register.filter
 def multiply(value, arg):
@@ -9,3 +12,22 @@ def multiply(value, arg):
         return int(value) * int(arg)
     except (ValueError, TypeError):
         return 0
+
+
+@register.filter(name="abs_value")
+def abs_value(value):
+    """
+    Возвращает абсолютное значение для числовых аргументов.
+    Сохраняет тип Decimal, если он был исходно.
+    """
+    if value is None:
+        return 0
+
+    try:
+        return abs(value)
+    except TypeError:
+        try:
+            decimal_value = Decimal(str(value))
+        except (InvalidOperation, ValueError):
+            return value
+        return abs(decimal_value)
