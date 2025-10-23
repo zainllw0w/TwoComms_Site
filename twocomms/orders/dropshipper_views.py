4db1f8977d4a1c300c061d03b7fa48c4e2f3b2a4
zainllw0w <pilimcenu270795@gmail.com>
2025-10-23 08:23:11 +0300

    Fix: Add missing staff_member_required import

--- twocomms/orders/dropshipper_views.py
+++ twocomms/orders/dropshipper_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
