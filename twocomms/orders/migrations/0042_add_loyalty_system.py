7b16882860f9e4484be8872d8ed05be08791ecd3
zainllw0w <pilimcenu270795@gmail.com>
2025-10-23 18:23:56 +0300

    Add loyalty system: -10 UAH per successful order (max -120 UAH), Telegram notifications for payouts

---
+++ twocomms/orders/migrations/0042_add_loyalty_system.py
@@ -0,0 +1,23 @@
+# Generated manually
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0041_dropshipperorder_payout_processed'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='dropshipperstats',
+            name='successful_orders',
+            field=models.PositiveIntegerField(default=0, verbose_name='Успішних замовлень (отримано)'),
+        ),
+        migrations.AddField(
+            model_name='dropshipperstats',
+            name='loyalty_discount',
+            field=models.DecimalField(decimal_places=2, default=0, max_digits=6, verbose_name='Знижка лояльності (грн)'),
+        ),
+    ]
+
