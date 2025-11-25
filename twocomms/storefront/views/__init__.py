14a4c473ac6e647ffc7ea281a59544502973bb0b
zainllw0w <pilimcenu270795@gmail.com>
2025-11-25 03:50:10 +0200

    Expose admin_reorder_products in storefront views

--- twocomms/storefront/views/__init__.py
+++ twocomms/storefront/views/__init__.py
@@ -196,6 +196,7 @@ from .admin import (
     manage_products,
     add_product,
     admin_product_builder,
+    admin_reorder_products,
     add_category,
     add_print,
     manage_print_proposals,
@@ -372,6 +373,7 @@ __all__ = [
     'admin_panel', 'admin_dashboard', 'manage_products', 'add_product', 'add_category', 'add_print',
     'manage_print_proposals', 'manage_promo_codes', 'generate_seo_content',
     'generate_alt_texts', 'manage_orders', 'sales_statistics', 'inventory_management',
+    'admin_reorder_products',
     
     # Aliases (для обратной совместимости)
     'cart', 'cart_remove', 'clean_cart', 'profile_setup_db', 'register_view_new',
