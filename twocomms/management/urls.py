95f2c7996c4442ed9a8ea2fc768d27c8556876aa
zainllw0w <pilimcenu270795@gmail.com>
2025-12-10 03:57:20 +0200

    feat: Implement management subdomain infrastructure and Nexus OS design

---
+++ twocomms/management/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomeView, ClientCreateView
+
+app_name = 'management'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('api/clients/create/', ClientCreateView.as_view(), name='client-create'),
+]
