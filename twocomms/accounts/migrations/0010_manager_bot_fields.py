cdaa40522b112a480dcd0c5a24d64bb34124938f
zainllw0w <pilimcenu270795@gmail.com>
2025-12-11 23:17:33 +0200

    Add Telegram Management bind UI and profile modal

---
+++ twocomms/accounts/migrations/0010_manager_bot_fields.py
@@ -0,0 +1,31 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0009_userprofile_payment_details'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='tg_manager_bind_code',
+            field=models.CharField(blank=True, max_length=64, verbose_name='Код привʼязки менеджмент-бота'),
+        ),
+        migrations.AddField(
+            model_name='userprofile',
+            name='tg_manager_bind_expires_at',
+            field=models.DateTimeField(blank=True, null=True, verbose_name='Діє до'),
+        ),
+        migrations.AddField(
+            model_name='userprofile',
+            name='tg_manager_chat_id',
+            field=models.BigIntegerField(blank=True, null=True, verbose_name='Telegram Management Chat ID'),
+        ),
+        migrations.AddField(
+            model_name='userprofile',
+            name='tg_manager_username',
+            field=models.CharField(blank=True, max_length=255, verbose_name='Telegram Management Username'),
+        ),
+    ]
