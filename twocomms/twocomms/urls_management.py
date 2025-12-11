59c9c64a9c6b6c1610ca86cd0dc049aa4ab5e649
zainllw0w <pilimcenu270795@gmail.com>
2025-12-11 09:04:17 +0200

    Include social auth URLs in management urlconf

--- twocomms/twocomms/urls_management.py
+++ twocomms/twocomms/urls_management.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('oauth/', include('social_django.urls', namespace='social')),
     path('', include('management.urls')),
 ]
 
