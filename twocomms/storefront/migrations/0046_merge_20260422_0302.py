99331d6e2eb009daf7d481da90f724d8c4923639
zainllw0w <pilimcenu270795@gmail.com>
2026-04-22 02:56:52 +0300

    Merge storefront push and analytics migrations

---
+++ twocomms/storefront/migrations/0046_merge_20260422_0302.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("storefront", "0045_pushnotificationcampaign_webpushdevicesubscription_and_more"),
+        ("storefront", "0045_sitesession_first_touch_data_sitesession_visitor_id_and_more"),
+    ]
+
+    operations = []
