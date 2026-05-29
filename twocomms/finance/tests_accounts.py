7ef27b3b8a8462703befc62768f79e0f549df44c
zainllw0w <pilimcenu270795@gmail.com>
2026-05-29 21:01:48 +0300

    test(finance): accounts page render + create API tests

--- twocomms/finance/tests_accounts.py
+++ twocomms/finance/tests_accounts.py
@@ -67,3 +67,34 @@ class ImportServiceTests(TestCase):
         result = import_service.import_rows(rows2, user=self.user, account=self.acc, apply_rules=False)
         self.assertEqual(result['created'], 0)
         self.assertEqual(result['skipped'], 2)
+
+
+class AccountsPageTests(TestCase):
+    FIN_HOST = 'fin.twocomms.shop'
+
+    def setUp(self):
+        self.user = User.objects.create_superuser('fin_p', 'p@x.com', 'x')
+        self.company = get_default_company()
+        Account.objects.create(company=self.company, name='Каса', currency='UAH',
+                               initial_balance=Decimal('100'), current_balance=Decimal('100'))
+        self.client.force_login(self.user)
+
+    def test_accounts_page_renders(self):
+        from django.test import override_settings
+        with override_settings(ALLOWED_HOSTS=['fin.twocomms.shop', 'testserver']):
+            resp = self.client.get('/accounts/', HTTP_HOST=self.FIN_HOST)
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Рахунки')
+        self.assertContains(resp, 'Оберіть спосіб додавання')
+
+    def test_account_create_api(self):
+        from django.test import override_settings
+        import json as _json
+        with override_settings(ALLOWED_HOSTS=['fin.twocomms.shop', 'testserver']):
+            resp = self.client.post('/api/accounts/create/',
+                                    data=_json.dumps({'name': 'Нова картка', 'currency': 'USD',
+                                                      'initial_balance': '300'}),
+                                    content_type='application/json', HTTP_HOST=self.FIN_HOST)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.json()['ok'])
+        self.assertTrue(Account.objects.filter(name='Нова картка', currency='USD').exists())
