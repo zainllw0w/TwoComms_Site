e40db100b0814ab611554fd25638be0b0732e729
zainllw0w <pilimcenu270795@gmail.com>
2026-07-31 01:23:43 +0300

    test(ig): align category UI rules contract

--- twocomms/management/tests_ig_category_ui.py
+++ twocomms/management/tests_ig_category_ui.py
@@ -18,7 +18,7 @@ class InteractionCategoryUiContractTests(SimpleTestCase):
         self.assertEqual(_interaction_tone(""), "neutral")
 
     def test_ui_contract_uses_current_rules_version(self):
-        self.assertEqual(ANALYSIS_RULES_VERSION, "2026-07-24.v4")
+        self.assertEqual(ANALYSIS_RULES_VERSION, "2026-07-26.v5")
 
     def test_overview_explanation_uses_runtime_model_truth(self):
         template = (
